57c2b6cf10292e19941a651491232c9331565d49
Tarak Blah <halbkarat@gmail.com>
2019-06-23 19:46:52 +0200

    Fix variable type assignment.

--- variables.tf
+++ variables.tf
@@ -24,43 +24,52 @@ variable "chart_enabled_nginx_ingress" {
 }
 
 variable "chart_namespace_cert_manager" {
-  type    = bool
+  type    = string
   default = "cert-manager"
 }
 
 variable "chart_namespace_external_dns" {
+  type    = string
   default = "external-dns"
 }
 
 variable "chart_namespace_kubernetes_dashboard" {
+  type    = string
   default = "kube-system"
 }
 
 variable "chart_namespace_metrics_server" {
+  type    = string
   default = "kube-system"
 }
 
 variable "chart_namespace_nginx_ingress" {
+  type    = string
   default = "nginx-ingress"
 }
 
 variable "chart_version_cert_manager" {
+  type    = string
   default = "v0.5.2"
 }
 
 variable "chart_version_external_dns" {
+  type    = string
   default = "1.6.0"
 }
 
 variable "chart_version_kubernetes_dashboard" {
+  type    = string
   default = "1.2.0"
 }
 
 variable "chart_version_metrics_server" {
+  type    = string
   default = "2.2.0"
 }
 
 variable "chart_version_nginx_ingress" {
+  type    = string
   default = "1.2.2"
 }
 
